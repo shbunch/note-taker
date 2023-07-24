a9bd9f190b6c56ec382cbaaccc492e935b12a5a7
shbunch <shawnbunch@gmail.com>
2023-07-24 01:26:19 -0500

    Added universal link for index.html page

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,10 @@ const PORT = 3001;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+)
+
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 )
