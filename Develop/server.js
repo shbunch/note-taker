3839bf6024a3540161b5d3867cfc7ef1f484b411
shbunch <shawnbunch@gmail.com>
2023-07-24 02:18:23 -0500

    Updated index code link, attempted to show json data

--- Develop/server.js
+++ Develop/server.js
@@ -3,16 +3,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3001;
+// Importing notes section
+const notes = require('./db/db.json')
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
-)
+);
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-)
+    res.sendFile(path.join(__dirname, 'public/notes.html')),
+    res.json(notes)
+);
 
 app.listen(PORT, () => 
     console.log(`Example app listening at http://localhost:${PORT}`)
