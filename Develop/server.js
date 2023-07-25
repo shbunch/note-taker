90cf1b9ea8373c2d101ab61862ad11e9b3124651
shbunch <shawnbunch@gmail.com>
2023-07-25 17:44:16 -0500

    Added variable for port

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // Importing notes
 const notes = require('./db/db.json')
 
